9a77d3f637f3267c572ec9304d1eef469b79b40f
Sebastian Lemus Cadena <sebastian.slc9@gmail.com>
2022-10-07 08:54:39 -0500

    Completes chapter 3

--- build.sbt
+++ build.sbt
@@ -1,7 +1,9 @@
 ThisBuild / scalaVersion  := "2.13.8"
 
 val catsVersion = "2.8.0"
+val catsEffectVersion = "3.3.14"
 
 libraryDependencies ++= Seq(
-  "org.typelevel" %% "cats-core" % "2.8.0"
+  "org.typelevel" %% "cats-core" % catsVersion,
+  "org.typelevel" %% "cats-effect" % catsEffectVersion
 )
---
+++ src/main/scala/chapter2/TickingClock.scala
@@ -0,0 +1,25 @@
+package chapter2
+
+import cats.effect.{IOApp, IO, ExitCode}
+import scala.concurrent.duration._
+import java.time.LocalDateTime
+import scala.util.Try
+
+object TickingClock extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    tickingClock.as(ExitCode.Success)
+
+  val getCurrentTime: IO[LocalDateTime] = 
+    IO { LocalDateTime.now() }
+
+  val tickingClock: IO[Unit] = {
+    val printCurrentTime = getCurrentTime.flatMap { t => 
+      val h = t.getHour
+      val m = t.getMinute
+      val s = t.getSecond
+      IO.println(s"Current Time: $h:$m:$s")  
+    }
+
+    (printCurrentTime >> IO.sleep(1.second)).foreverM.void
+  } // <1>
+}
