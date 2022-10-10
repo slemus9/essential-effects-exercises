933dd1cca04cf2d1c7e6792a59283d4f6cdeaa01
Sebastian Lemus Cadena <sebastian.slc9@gmail.com>
2022-10-10 11:09:45 -0500

    Fixes custom parMapN implementation

--- src/main/scala/chapter4/ParMapN.scala
+++ src/main/scala/chapter4/ParMapN.scala
@@ -1,8 +1,10 @@
 package chapter4
 
-import cats.syntax.apply._
-import cats.effect.{IO, Outcome, OutcomeIO, FiberIO}
+import cats.syntax.all._
+import cats.effect.{IO, IOApp, Outcome, OutcomeIO, FiberIO}
 import Outcome.{Succeeded, Canceled, Errored}
+import utils.debug._
+import scala.concurrent.duration._
 
 /*
   - Start both computations so they run concurrently ("fork them")
@@ -10,9 +12,9 @@ import Outcome.{Succeeded, Canceled, Errored}
   - If one effect fails, cancel the other one
   - Combine the results with the given f function
 */
-object ParMapN {
+object ParMapN extends IOApp.Simple {
 
-  private def parMapNWrong [A, B, C] (
+  private def parMapNWrong1 [A, B, C] (
     fa: IO[A], fb: IO[B]
   ) (f: (A, B) => C): IO[C] =
     for {
@@ -26,7 +28,7 @@ object ParMapN {
                       // which is not what we want
     } yield ???
 
-  def parMapN [A, B, C] (
+  def parMapNWrong2 [A, B, C] (
     fa: IO[A], fb: IO[B]
   ) (f: (A, B) => C): IO[C] = {
 
@@ -36,7 +38,7 @@ object ParMapN {
 
     // Every flatMap is treated as a cancelation boundary
     IO.racePair(fa, fb).flatMap {
-      case Left(outA -> fiberB)  => fiberB.join.flatMap { 
+      case Left(outA -> fiberB)  => fiberB.join.flatMap { // fiberB will join regardless of the outcome of fa
         (outA, _).mapN(f).embed(raiseCancelationError)
       }
       case Right(fiberA -> outB) => fiberA.join.flatMap { 
@@ -45,24 +47,22 @@ object ParMapN {
     }
   }
 
-  def parMapNAlt [A, B, C] (
+  def parMapN [A, B, C] (
     fa: IO[A], fb: IO[B]
   ) (f: (A, B) => C): IO[C] = {
-    
-    val raiseCancelationError = IO.raiseError(
-      new InterruptedException("A fiber was cancelled")
-    )
 
     def handleRaceOutcomeWith [A, B, C] (f: (A, B) => C) (
       outA: OutcomeIO[A], fiberB: FiberIO[B]
-    ) = outA match {
-      case Canceled()    => fiberB.cancel >> raiseCancelationError
-      case Errored(e)    => IO.raiseError(e)
-      case Succeeded(fa) => 
-        ( fa
-        , fiberB.joinWith(raiseCancelationError)
-        ).mapN(f)
-    }
+    ): IO[C] =       
+      outA match {
+        case Canceled()    => fiberB.cancel >> IO.canceled >> IO.never
+        case Errored(e)    => fiberB.cancel >> IO.raiseError(e)
+        case Succeeded(fa) => 
+          ( fa
+          , fiberB.joinWith(IO.canceled >> IO.never)
+          ).mapN(f)
+      }
+    
 
     val handlerA = handleRaceOutcomeWith (f) _
     val handlerB = handleRaceOutcomeWith { (b: B, a: A) => f(a, b) } _
@@ -71,5 +71,53 @@ object ParMapN {
       case Left(outA -> fiberB)  => handlerA(outA, fiberB)
       case Right(fiberA -> outB) => handlerB(outB, fiberA)
     }
+  }
+
+
+  def run: IO[Unit] = {
+    
+    val t1 = 
+      IO.println("Starting Task1") *> 
+      IO.sleep(4.seconds)          *> 
+      IO.pure(4)                   <*
+      IO.println("Task1 finished")
+
+    val t2 = 
+      IO.println("Starting Task2") *>
+      IO.sleep(2.seconds)          *>
+      IO.pure(2)                   <*
+      IO.println("Task2 finised")
+
+    val t2Error: IO[Int] = 
+      IO.println("Starting Task2") *>
+      IO.sleep(2.seconds)          *>
+      IO.raiseError(new Exception("Task 2 failed")) <*
+      IO.println("Task2 finised")
+
+    val t2Cancelled =
+      IO.println("Starting Task2") *> 
+      IO.sleep(2.seconds)          *>
+      IO.canceled                  *>
+      IO.pure(2)                   <*
+      IO.println("Task2 finished")
+
+    val t3 = parMapN (t1.debug, t2.debug) (_ + _)
+
+    val t4 = parMapN (
+      t1.debug.onCancel(IO.println("Task 1 cancelled")), 
+      t2Error.debug
+    ) (_ + _)
+
+    val t5 = parMapN (
+      t1.debug.onCancel(IO.println("Task 1 cancelled")), 
+      t2Cancelled.debug.onCancel(IO.println("Task 2 cancelled"))
+    ) (_ + _)
+
+    // t3.flatMap(IO.println)
+    // t4.flatMap(IO.println)
+    // t5.flatMap(IO.println)
+    // (t1.debug, t2Cancelled.debug)
+    //   .parMapN(_ + _)
+    //   .flatMap(IO.println)
   }
 }
